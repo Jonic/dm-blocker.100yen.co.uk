f1809ac61fae2b69ab90083686f7b4e71767ca14
Jonic Linley <jonic@100yen.co.uk>
2013-05-01 15:58:47 +0100

    Initial content and design and that done. I need to get the cat gifs at some point, but this will do for now :)

--- index.php
+++ index.php
@@ -147,8 +147,16 @@
 				padding: 0;
 			}
 
+			a {
+				color: #d35400;
+			}
+
 			html, body {
+				background: #000;
+				color: #333;
+				font-family: Helvetica, Arial, sans-serif;
 				height: 100%;
+				overflow: hidden;
 				width: 100%;
 			}
 
@@ -157,10 +165,46 @@
 				height: 100%;
 				width: 100%;
 			}
+
+			.psa {
+				background: rgba(255, 255, 255, 0.75);
+				bottom: 0;
+				box-sizing: border-box;
+				padding: 1em 2em;
+				position: absolute;
+				width: 40%;
+			}
+
+				.psa h1 {
+					font-size: 2em;
+					margin-bottom: 0.5em;
+				}
+
+				.psa p {
+					line-height: 1.4;
+					margin-bottom: 1em;
+				}
+
+					.psa p:last-child {
+						margin-bottom: 0;
+					}
 		</style>
 	</head>
 
 	<body>
+		<div class="psa">
+			<h1>Hang on a second!</h1>
+
+			<p>
+				You're heading towards an article that was probably written specifically to provoke a reaction. Take a moment, enjoy this gif, and think about whether or
+				not you want to give them the satisfaction of knowing that you took the bait.
+			</p>
+
+			<p>
+				Another selfless act performed just for you by <a href="http://twitter.com/Jonic">@Jonic</a>. You're <em>welcome</em>.
+			</p>
+		</div>
+
 		<img alt="" src="http://procatinator.com/images/<?php echo $cats[$cat]; ?>" />
 	</body>
 </html>
