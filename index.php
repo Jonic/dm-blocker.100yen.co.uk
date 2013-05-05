118a00c4bf42584cfb4fe22637d88e2d275e6557
Jonic Linley <jonic@100yen.co.uk>
2013-05-05 20:03:38 +0100

    Added Gaug.es tracking code

--- index.php
+++ index.php
@@ -75,5 +75,19 @@
 		</div>
 
 		<img alt="" src="/images/<?php echo $cat_file; ?>.gif" />
+
+		<script type="text/javascript">
+		  var _gauges = _gauges || [];
+		  (function() {
+		    var t   = document.createElement('script');
+		    t.type  = 'text/javascript';
+		    t.async = true;
+		    t.id    = 'gauges-tracker';
+		    t.setAttribute('data-site-id', '5186ac12613f5d3c86000002');
+		    t.src = '//secure.gaug.es/track.js';
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(t, s);
+		  })();
+		</script>
 	</body>
 </html>
