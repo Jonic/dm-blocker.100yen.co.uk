c8edc58f7951358487a9725a42a7f967a8556d4b
Jonic Linley <jonic@100yen.co.uk>
2014-12-07 00:14:51 +0000

    Added new gifs - tweaked the design of the page.

--- index.php
+++ index.php
@@ -1,58 +1,68 @@
 <?php
 
-	$num_cats = 130;
-	$cat_file = rand(1, 130);
+	$num_cats = 185;
+	$cat_file = rand(1, $num_cats);
 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>STOP</title>
+		<title>Daily Mail Blocker</title>
 
 		<meta name="google-site-verification" content="EJOTQWsVwuMA6tjtdmDXM0v8ejx4G12gMnu9MKnT5f4" />
 
 		<style>
 			* {
-				border: 0;
-				margin: 0;
+				border:  0;
+				margin:  0;
 				padding: 0;
 			}
 
 			a {
-				color: #d35400;
+				color: #3C8FC7;
 			}
 
 			html, body {
-				background: #000;
-				color: #333;
+				background:  #000;
+				color:       #333;
 				font-family: Helvetica, Arial, sans-serif;
-				height: 100%;
-				overflow: hidden;
-				width: 100%;
+				height:      100%;
+				overflow:    hidden;
+				width:       100%;
 			}
 
 			img {
 				display: block;
-				height: 100%;
-				width: 100%;
+				height:  100%;
+				width:   100%;
 			}
 
 			.psa {
-				background: rgba(255, 255, 255, 0.75);
-				bottom: 0;
-				box-sizing: border-box;
-				padding: 1em 2em;
-				position: absolute;
-				width: 40%;
+				-webkit-transform: translateX(-50%);
+				   -moz-transform: translateX(-50%);
+				    -ms-transform: translateX(-50%);
+				     -o-transform: translateX(-50%);
+
+				background:    rgba(255, 255, 255, 0.5);
+				bottom:        0;
+				border-radius: 1em 1em 0 0;
+				box-sizing:    border-box;
+				left:          50%;
+				max-width:     600px;
+				padding:       1em 1em;
+				position:      absolute;
+				transform:     translateX(-50%);
+				width:         90%;
 			}
 
 				.psa h1 {
-					font-size: 2em;
+					font-size:     1.5em;
 					margin-bottom: 0.5em;
+					text-align:    center;
 				}
 
 				.psa p {
-					line-height: 1.4;
+					line-height:   1.4;
 					margin-bottom: 1em;
 				}
 
@@ -67,12 +77,14 @@
 			<h1>Hang on a second!</h1>
 
 			<p>
-				You're heading towards an article that was probably written specifically to provoke a reaction. Take a moment, enjoy this gif, and think about whether or
-				not you want to give them the satisfaction of knowing that you took the bait.
+				You were headed to the Daily Mail's website. Have a look at this
+				cat gif instead. Refresh for another, if you like.
 			</p>
 
 			<p>
-				Another selfless act performed just for you by <a href="http://twitter.com/Jonic">@Jonic</a>. You're <em>welcome</em>.
+				Another selfless act performed just for you by
+				<a href="http://twitter.com/Jonic">@Jonic</a>.
+				You're <em>welcome</em>.
 			</p>
 		</div>
 
