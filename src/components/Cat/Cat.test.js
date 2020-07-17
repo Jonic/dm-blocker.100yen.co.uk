9a2df2f1a562a0ca52f39703e707a6c6eb919c72
Jonic Linley <jonic@100yen.co.uk>
2020-07-17 11:00:43 +0100

    Why the heck was that using React.

--- src/components/Cat/Cat.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Cat from './Cat';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Cat />, div);
-});
